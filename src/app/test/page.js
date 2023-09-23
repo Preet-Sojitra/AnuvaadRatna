2a4a12d475f197469f489459cad230e33c865c33
Preet Sojitra <preet.dev373@gmail.com>
2023-09-23 21:28:44 +0530

    thoda thoda progress hoti

---
+++ src/app/test/page.js
@@ -0,0 +1,45 @@
+"use client"
+
+import React, { useState } from "react"
+import axios from "axios"
+
+function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState(null)
+  console.log(selectedFile)
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0])
+  }
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      const formData = new FormData()
+      formData.append("file", selectedFile)
+
+      axios
+        .post("http://127.0.0.1:5000/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data", // Important for sending files
+          },
+        })
+        .then((response) => {
+          // Handle the response from the server
+          console.log(response.data)
+        })
+        .catch((error) => {
+          // Handle any errors
+          console.error(error)
+        })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Upload a File</h2>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  )
+}
+
+export default FileUpload
