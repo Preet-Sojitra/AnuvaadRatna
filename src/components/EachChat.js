68486cc04711ae70cbdbdd10d5aa350dccac8657
YugBhanushali <yughbhanushali241103@gmail.com>
2023-09-24 13:56:00 +0530

    added loader and animation

--- src/components/EachChat.js
+++ src/components/EachChat.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Typewriter from "typewriter-effect"
-
+import { PulseLoader} from "react-spinners"
 const EachChat = (props) => {
   // console.log(chat);
   //   console.log(props)
+  
   return (
     <div className="flex flex-col z-10">
       <div className="flex justify-end">
@@ -16,7 +17,19 @@ const EachChat = (props) => {
       <div className="flex justify-start">
         {props?.computer !== null ? (
           <div className="flex justify-start text-justify bg-white py-2 px-4 text-black items-center rounded-md mx-2 mt-2 max-w-[400px]">
-            {props?.computer}
+            {
+              props?.computer === "Loading..." ? 
+                <PulseLoader color="#3b82f6" size={10} margin={3} /> 
+              : 
+                <Typewriter
+                    options={{
+                    strings: "इस रिपोर्ट में हम आपको कुछ खास",
+                    autoStart: true,
+                    loop: false,
+                    delay:50
+                  }}
+                />
+             }
           </div>
         ) : null}
       </div>
